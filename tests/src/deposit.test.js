98b5b5bd761cf3c71c11237c8123f24b400d7982
s. rannou <mxs@sbrk.org>
2022-10-07 12:03:12 +0200

    fix: handle resolution in unit tests

--- tests/src/deposit.test.js
+++ tests/src/deposit.test.js
@@ -1,6 +1,6 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
-import { waitForAppScreen, zemu, genericTx, nano_models,SPECULOS_ADDRESS, txFromEtherscan} from './test.fixture';
+import { waitForAppScreen, kilnJSON, zemu, genericTx, nano_models,SPECULOS_ADDRESS, txFromEtherscan} from './test.fixture';
 import { ethers } from "ethers";
 import { parseEther, parseUnits} from "ethers/lib/utils";
 import { ledgerService } from '@ledgerhq/hw-app-eth';
@@ -24,7 +24,10 @@ nano_models.forEach(function(model) {
         unsignedTx.value = parseEther("32");
 
         const serializedTx = ethers.utils.serializeTransaction(unsignedTx).slice(2);
-        const tx = eth.signTransaction("44'/60'/0'/0", serializedTx);
+        const resolution = await ledgerService.resolveTransaction(serializedTx, eth.loadConfig, {
+          externalPlugins: true,
+        })
+        const tx = eth.signTransaction("44'/60'/0'/0", serializedTx, resolution);
         const right_clicks = 4;
 
         await waitForAppScreen(sim);
--- tests/src/withdraw.test.js
+++ tests/src/withdraw.test.js
@@ -3,6 +3,7 @@ import "regenerator-runtime/runtime";
 import { waitForAppScreen, zemu, genericTx, nano_models,SPECULOS_ADDRESS, txFromEtherscan} from './test.fixture';
 import { ethers } from "ethers";
 import { parseEther, parseUnits} from "ethers/lib/utils";
+import { ledgerService } from '@ledgerhq/hw-app-eth';
 
 const contractAddr = "0xe8ff2a04837aac535199eecb5ece52b2735b3543";
 
@@ -26,7 +27,10 @@ nano_models.forEach(function(model) {
         unsignedTx.data = data;
 
         const serializedTx = ethers.utils.serializeTransaction(unsignedTx).slice(2);
-        const tx = eth.signTransaction("44'/60'/0'/0", serializedTx);
+        const resolution = await ledgerService.resolveTransaction(serializedTx, eth.loadConfig, {
+          externalPlugins: true,
+        })
+        const tx = eth.signTransaction("44'/60'/0'/0", serializedTx, resolution);
         const right_clicks = 5;
 
         await waitForAppScreen(sim);
