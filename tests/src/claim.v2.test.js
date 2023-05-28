650479a649effcde15ecb3c778d5c239058ae154
pwnh4 (@loicttn) <loic@ttn.am>
2023-05-28 17:30:09 +0200

    feat: tests for v2 interactions

--- tests/src/claim.v2.test.js
+++ tests/src/claim.v2.test.js
@@ -1,19 +1,11 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import {
-  waitForAppScreen,
-  kilnJSON,
-  zemu,
-  genericTx,
-  nano_models,
-  SPECULOS_ADDRESS,
-  txFromEtherscan,
-} from './test.fixture';
+import { waitForAppScreen, zemu, genericTx, nano_models } from './test.fixture';
 import { ethers } from 'ethers';
-import { parseEther, parseUnits } from 'ethers/lib/utils';
+import { parseEther } from 'ethers/lib/utils';
 import { ledgerService } from '@ledgerhq/hw-app-eth';
 
-const contractAddr = '0xe8ff2a04837aac535199eecb5ece52b2735b3543';
+const contractAddr = '0xb0c8078af582d8b754ef35f3bdaceff8587bd914';
 
 const pluginName = 'kiln';
 const abi_path = `../${pluginName}/abis/` + contractAddr + '.json';
@@ -21,17 +13,21 @@ const abi = require(abi_path);
 
 nano_models.forEach(function (model) {
   test(
-    '[Nano ' + model.letter + '] Stake Eth',
+    '[Nano ' + model.letter + '] Claim V2 Eth',
     zemu(model, async (sim, eth) => {
       const contract = new ethers.Contract(contractAddr, abi);
 
-      const { data } = await contract.populateTransaction.deposit();
+      const { data } = await contract.populateTransaction.claim(
+        [150, 2],
+        [0, 1],
+        132
+      );
 
       let unsignedTx = genericTx;
 
       unsignedTx.to = contractAddr;
       unsignedTx.data = data;
-      unsignedTx.value = parseEther('32');
+      unsignedTx.value = parseEther('0');
 
       const serializedTx = ethers.utils
         .serializeTransaction(unsignedTx)
@@ -47,7 +43,7 @@ nano_models.forEach(function (model) {
       const right_clicks = 4;
 
       await waitForAppScreen(sim);
-      await sim.navigateAndCompareSnapshots('.', model.name + '_deposit', [
+      await sim.navigateAndCompareSnapshots('.', model.name + '_claimv2', [
         right_clicks,
         0,
       ]);
