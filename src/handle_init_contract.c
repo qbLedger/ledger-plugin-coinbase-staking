46ea3056c42b195ecc19d3c05230543d694c9934
pwnh4 (@loicttn) <loic@ttn.am>
2024-05-29 10:21:06 +0200

    fix: I2

--- src/handle_init_contract.c
+++ src/handle_init_contract.c
@@ -40,6 +40,13 @@ void handle_init_contract(ethPluginInitContract_t *msg) {
     }
     context->selectorIndex = index;
 
+    // check for overflow
+    if ((size_t) context->selectorIndex != index) {
+        PRINTF("Error: overflow detected on selector index!\n");
+        msg->result = ETH_PLUGIN_RESULT_ERROR;
+        return;
+    }
+
     msg->result = ETH_PLUGIN_RESULT_OK;
 
     switch (context->selectorIndex) {
